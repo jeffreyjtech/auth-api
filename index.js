3dbac7c45915dce6b19b48715f184c0e9774b106
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 00:07:45 -0700

    Added console.log confirming DB sync

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = require('./src/server.js');
 const { db } = require('./src/models');
 
 db.sync().then(() => {
-  app.start(process.env.PORT || 3001);
+  console.log('Database synced');
+  app.start(process.env.PORT || 3000);
 });
