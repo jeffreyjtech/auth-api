f8765ac35822206a56eb92ef9c6a0e3a45aa0d97
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 00:56:48 -0700

    Added dynamic status and message

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function handle500(err, req, res, next) {
-  console.error('Server Error');
-  console.error(err.message);
-  res.status(500).send('Server Error');
+  console.error('Server Error:\n  ');
+  console.error(err.message || 'Unknown error');
+  res.status(err.status || 500).send(err.message || 'Server Error');
 }
 
 module.exports = handle500;
