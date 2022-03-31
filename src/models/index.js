142149f3df6d9c3e69e3c51cf75f7b29acbf586c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 00:57:07 -0700

    Tweaked userModel import and export statements

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const userModel = require('./users/model.js');
+const userModel = require('../auth/users-model.js');
 
 const clothesModel = require('./clothes/model.js');
 const foodModel = require('./food/model.js');
@@ -13,11 +13,10 @@ const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 const sequelize = new Sequelize(DATABASE_URL);
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
-const users = userModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
   food: new Collection(food),
   clothes: new Collection(clothes),
-  users,
+  users: userModel(sequelize, DataTypes),
 };
